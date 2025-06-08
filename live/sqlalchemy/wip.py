840433fbc4cfadb8edf2a2f1e238ad069d685fc5
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-08 02:30:00 -0300

    feito conecção com postgres criado engine

---
+++ live/sqlalchemy/wip.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+import psycopg2  # Importe explicitamente o psycopg2
+
+# try:
+engine = create_engine(
+    "postgresql+psycopg2://postgres:D%40vi0406@localhost:5432/faculdade"
+    # 'sqlite://'
+)
+print(engine)
+print(engine.dialect)
+    # Tente uma conexão de teste para verificar se tudo está funcionando
+#     with engine.connect() as connection:
+#         print("Conexão bem-sucedida!")
+# except ImportError as e:
+#     print(f"Erro ao importar psycopg2: {e}")
+# except Exception as e:
+#     print(f"Erro ao conectar ao banco de dados: {e}")
