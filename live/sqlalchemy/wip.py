6a0a3091fc61a4a1e7440773e06e21843d731ee0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-08 02:47:12 -0300

    abrindo e fechando conexões

--- live/sqlalchemy/wip.py
+++ live/sqlalchemy/wip.py
@@ -1,24 +1,22 @@
 from sqlalchemy import create_engine
 import psycopg2  # Importe explicitamente o psycopg2
 
-# try:
 engine = create_engine(
     "postgresql+psycopg2://postgres:D%40vi0406@localhost:5432/faculdade",
     # 'sqlite://'
     echo=True,
 )
-print(engine)
-print(engine.dialect)
-    # Tente uma conexão de teste para verificar se tudo está funcionando
-#     with engine.connect() as connection:
-#         print("Conexão bem-sucedida!")
-# except ImportError as e:
-#     print(f"Erro ao importar psycopg2: {e}")
-# except Exception as e:
-#     print(f"Erro ao conectar ao banco de dados: {e}")
+print(engine.pool)
 
-conn = engine.connect()
+conn1 = engine.connect()
+conn2 = engine.connect()
+conn1.close()
+conn2.close()
+conn3 = engine.connect()
+
+print(engine.pool.status())
+
 
 # print(conn.connection.dbapi_connection)
 
-conn.close()
+# conn.close()
