4ece9f374532ba1816773a63ac6da6611c4226e0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-08 03:10:31 -0300

    Adicionando a comentarios para estudo

--- live/sqlalchemy/wip.py
+++ live/sqlalchemy/wip.py
@@ -1,16 +1,21 @@
 from sqlalchemy import create_engine, text
 import psycopg2  # Importe explicitamente o psycopg2
 
+# CRIADO A ENGINE DE CONEXÃO
 engine = create_engine(
     "postgresql+psycopg2://postgres:D%40vi0406@localhost:5432/faculdade",
     # 'sqlite://'
     echo=True,
 )
 
+# CRIADO A VARIAVEL DE CONEXÃO CON
 con = engine.connect()
 
+# COMANDO SQL 
 sql = text('select * from usuarios')
 
+# EXECUTANDO O COMANDO SQL NO BANCO CONECTADO
 con.execute(sql)
 
+# FECHANDO A CONEXÃO
 con.close()
