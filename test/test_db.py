a0549309011a9c4dd1befaaf498003e8ecd9165b
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-10 00:04:18 -0300

    criado cenario de teste

--- test/test_db.py
+++ test/test_db.py
@@ -1,11 +1,29 @@
+from dataclasses import asdict
+from sqlalchemy import select
+from datetime import datetime
 from models import User
 
-def test_creat_user():
-    user = User(
-        username='test',
-        email='test@test',
-        password='secret'
-    )
 
+def test_creat_user(session, mock_db_time):
 
-    assert user.username == 'test'
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='test',
+            email='test@test',
+            password='secret'
+        )
+
+        session.add(new_user)
+        session.commit()
+
+        user = session.scalar(
+            select(User).where(User.username == 'test')
+        )
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'test',
+        'email': 'test@test',
+        'password': 'secret',
+        'created_at': time,
+    }
